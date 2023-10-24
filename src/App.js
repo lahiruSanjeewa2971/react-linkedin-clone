41fc2191c33287336f9e96144a0359d25cfd7cbe
IT19166834_K.S.L.K.Rathnasheela <it19166834@my.sliit.lk>
2023-10-24 16:19:18 +0530

    finish login section below nav bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Login from './components/Login';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path='/' element={<Login/>} />
+          <Route path="/" element={<Login />} />
+          <Route
+            path="/home"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
         </Routes>
       </Router>
     </div>
