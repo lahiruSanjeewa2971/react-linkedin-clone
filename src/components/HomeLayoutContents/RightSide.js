0bae5b3c352f15cb865feed972bd95332fddd9a0
IT19166834_K.S.L.K.Rathnasheela <it19166834@my.sliit.lk>
2023-10-31 16:40:06 +0530

    Finish right side component design

--- src/components/HomeLayoutContents/RightSide.js
+++ src/components/HomeLayoutContents/RightSide.js
@@ -5,9 +5,42 @@ const RightSide = (props) => {
     <Container>
       <FollowCard>
         <Title>
-            <h2>Add to your feed</h2>
+          <h2>Add to your feed</h2>
+          <img src="./images/feed-icon.svg" alt="" />
         </Title>
+
+        <FeedList>
+          <li>
+            <a>
+              <Avatar />
+            </a>
+            <div>
+              <span>#linkedin</span>
+              <button>Follow</button>
+            </div>
+          </li>
+
+          <li>
+            <a>
+              <Avatar />
+            </a>
+            <div>
+              <span>#Video</span>
+              <button>Follow</button>
+            </div>
+          </li>
+        </FeedList>
+        <Recommendation>
+          View all recomendations
+          <img src="./images/right-icon.svg" alt="" />
+        </Recommendation>
       </FollowCard>
+      <BannerCard>
+        <img
+          src="https://static-exp1.licdn.com/scds/common/u/images/promo/ads/li_evergreen_jobs_ad_300x250_v1.jpg"
+          alt=""
+        />
+      </BannerCard>
     </Container>
   );
 };
