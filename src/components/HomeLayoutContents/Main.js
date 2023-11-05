7efc2fced6d3700e5376f8b232887454deaa4345
IT19166834_K.S.L.K.Rathnasheela <it19166834@my.sliit.lk>
2023-11-06 00:42:36 +0530

    Starting react-redux part

--- src/components/HomeLayoutContents/Main.js
+++ src/components/HomeLayoutContents/Main.js
@@ -49,41 +49,87 @@ const Main = (props) => {
       </ShareBox>
       <div>
         <Article>
-            <SharedActor>
-                <a>
-                    <img src="./images/user.svg" alt="" />
-                    <div>
-                        <span>Title</span>
-                        <span>Info</span>
-                        <span>Date</span>
-                    </div>
-                </a>
-                <button>
-                    <img src="./images/ellipsis.png" alt="" style={{width: '18px'}} />
-                </button>
-            </SharedActor>
-            <Description>
-                discription
-            </Description>
-            <SharedImage>
-                <a>
-                    <img src="./images/shared-image.jpg" alt="" />
-                </a>
-            </SharedImage>
-            <SocialCounts>
-                <li>
-                    <button>
-                        <img src="./images/like-icon.png" alt="" />
-                        <img src="./images/clapping-icon.png" alt="" />
-                        <span>75</span>
-                    </button>
-                </li>
-                <li>
-                    <a>
-                        2 comment
-                    </a>
-                </li>
-            </SocialCounts>
+          <SharedActor>
+            <a>
+              <img src="./images/user.svg" alt="" />
+              <div>
+                <span>Title</span>
+                <span>Info</span>
+                <span>Date</span>
+              </div>
+            </a>
+            <button>
+              <img
+                src="./images/ellipsis.png"
+                alt=""
+                style={{ width: "18px" }}
+              />
+            </button>
+          </SharedActor>
+          <Description>discription</Description>
+          <SharedImage>
+            <a>
+              <img src="./images/shared-image.jpg" alt="" />
+            </a>
+          </SharedImage>
+          <SocialCounts>
+            <li>
+              <button>
+                <img
+                  src="./images/like-icon.png"
+                  alt=""
+                  style={{ width: "15px", height: "15px" }}
+                />
+                <img
+                  src="./images/clapping-icon.png"
+                  alt=""
+                  style={{ width: "15px", height: "15px" }}
+                />
+                <span>75</span>
+              </button>
+            </li>
+            <li>
+              <a>2 comment</a>
+            </li>
+          </SocialCounts>
+
+          <SocialActions>
+            <button>
+              <img
+                src="./images/thumbs-up.png"
+                alt=""
+                style={{ width: "25px", height: "25px" }}
+              />
+              <span>Like</span>
+            </button>
+
+            <button>
+              <img
+                src="./images/comments-icon.png"
+                alt=""
+                style={{ width: "25px", height: "25px" }}
+              />
+              <span>Comments</span>
+            </button>
+
+            <button>
+              <img
+                src="./images/share-icon.png"
+                alt=""
+                style={{ width: "25px", height: "25px" }}
+              />
+              <span>Share</span>
+            </button>
+
+            <button>
+              <img
+                src="./images/send-icon.png"
+                alt=""
+                style={{ width: "25px", height: "25px" }}
+              />
+              <span>Send</span>
+            </button>
+          </SocialActions>
         </Article>
       </div>
     </Container>
@@ -111,133 +157,169 @@ const ShareBox = styled(CommonCard)`
   color: #958b7b;
   margin: 0 0 8px;
   background: white;
-  div{
-    button{
-        outline: none;
-        color: rgba(0,0,0,0.6);
-        font-size: 14px;
-        line-height: 1.5%;
-        min-height: 48px;
-        background: transparent;
-        border: none;
-        display: flex;
-        align-items: center;
-        font-weight: 600;
+  div {
+    button {
+      outline: none;
+      color: rgba(0, 0, 0, 0.6);
+      font-size: 14px;
+      line-height: 1.5%;
+      min-height: 48px;
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+      font-weight: 600;
     }
-    &:first-child{
-        display: flex;
-        align-items: center;
-        padding: 8px 16px 0px 16px;
-        img{
-            width: 48px;
-            border-radius: 50%;
-            margin-right: 8px;
-        }
-        button{
-            margin: 4px 0;
-            flex-grow: 1;
-            border-radius: 35px;
-            padding-left: 16px;
-            border: 1px solid rgba(0, 0, 0, 0.16);
-            background-color: white;
-            text-align: left;
-        }
+    &:first-child {
+      display: flex;
+      align-items: center;
+      padding: 8px 16px 0px 16px;
+      img {
+        width: 48px;
+        border-radius: 50%;
+        margin-right: 8px;
+      }
+      button {
+        margin: 4px 0;
+        flex-grow: 1;
+        border-radius: 35px;
+        padding-left: 16px;
+        border: 1px solid rgba(0, 0, 0, 0.16);
+        background-color: white;
+        text-align: left;
+      }
     }
-    &:nth-child(2){
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-around;
-        padding-bottom: 4px;
-        button{
-            img {
-                margin: 0 4px 0 -2px;
-            }
-            span {
-                color: #70b5f9;
-            }
+    &:nth-child(2) {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+      padding-bottom: 4px;
+      button {
+        img {
+          margin: 0 4px 0 -2px;
         }
+        span {
+          color: #70b5f9;
+        }
+      }
     }
   }
 `;
 
 const Article = styled(CommonCard)`
-    padding: 0;
-    margin: 0 0 8px;
-    overflow: visible;
+  padding: 0;
+  margin: 0 0 8px;
+  overflow: visible;
 `;
 
 const SharedActor = styled.div`
-    padding-right: 40px;
-    flex-wrap: nowrap;
-    padding: 12px 16px 0;
-    margin-bottom: 8px;
-    align-items: center;
+  padding-right: 40px;
+  flex-wrap: nowrap;
+  padding: 12px 16px 0;
+  margin-bottom: 8px;
+  align-items: center;
+  display: flex;
+  a {
+    margin-right: 12px;
+    flex-grow: 1;
+    overflow: hidden;
     display: flex;
-    a{
-        margin-right: 12px;
-        flex-grow: 1;
-        overflow: hidden;
-        display: flex;
-        text-decoration: none;
+    text-decoration: none;
 
-        img{
-            width: 48px;
-            height: 48px;
-        }
-        & > div{
-            display: flex;
-            flex-direction: column;
-            flex-grow: 1;
-            flex-basis: 0;
-            margin-left: 8px;
-            overflow: hidden;
-            span{
-                text-align: left;
-
-                &:first-child{
-                    font-size: 14px;
-                    font-weight: 700;
-                    color: rgba(0,0,0,1);
-                }
-
-                &:nth-child(n+1){
-                    font-size: 12px;
-                    color: rgba(0,0,0,0.6);
-                }
-            }
-        }
+    img {
+      width: 48px;
+      height: 48px;
     }
-    button{
-        position: absolute;
-        right: 12px;
-        top: 0;
-        background: transparent;
-        border: none;
-        outline: none;
+    & > div {
+      display: flex;
+      flex-direction: column;
+      flex-grow: 1;
+      flex-basis: 0;
+      margin-left: 8px;
+      overflow: hidden;
+      span {
+        text-align: left;
+
+        &:first-child {
+          font-size: 14px;
+          font-weight: 700;
+          color: rgba(0, 0, 0, 1);
+        }
+
+        &:nth-child(n + 1) {
+          font-size: 12px;
+          color: rgba(0, 0, 0, 0.6);
+        }
+      }
     }
+  }
+  button {
+    position: absolute;
+    right: 12px;
+    top: 0;
+    background: transparent;
+    border: none;
+    outline: none;
+  }
 `;
 
 const Description = styled.div`
-    padding: 0 16px;
-    overflow: hidden;
-    color: rgba(0,0,0,0.9);
-    font-size: 14px;
-    text-align: left;
+  padding: 0 16px;
+  overflow: hidden;
+  color: rgba(0, 0, 0, 0.9);
+  font-size: 14px;
+  text-align: left;
 `;
 
 const SharedImage = styled.div`
-    margin-top: 8px;
+  margin-top: 8px;
+  width: 100%;
+  display: block;
+  position: relative;
+  background-color: #f9fafb;
+  img {
+    object-fit: contain;
     width: 100%;
-    display: block;
-    position: relative;
-    background-color: #f9fafb;
-    img{
-        object-fit: contain;
-        width: 100%;
-        height: 100%;
-    }
+    height: 100%;
+  }
 `;
 
-const SocialCounts = styled.div``;
+const SocialCounts = styled.ul`
+  line-height: 1.3;
+  display: flex;
+  align-items: flex-start;
+  overflow: auto;
+  margin: 0 16px;
+  padding: 8px 0;
+  border-bottom: 1px solid #e9e5df;
+  list-style: none;
+  li {
+    margin-right: 5px;
+    font-size: 12px;
+    button {
+      display: flex;
+    }
+  }
+`;
+
+const SocialActions = styled.div`
+    align-items: center;
+    display: flex;
+    justify-content: flex-start;
+    margin: 0;
+    padding: 4px 8px;
+    button{
+        display: inline-flex;
+        align-items: center;
+        padding: 8px;
+        color: #0a66c2;
+
+        @media (min-width: 768px){
+            span{
+                margin-left: 8px;
+            }
+        }
+    }
+`;
 
 export default Main;
